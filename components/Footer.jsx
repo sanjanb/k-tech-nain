d5834d5057a866fe394bc8ff994bd79e10968c7b
Sanjan B M <acharyasanjan931@gmail.com>
2025-12-17 09:51:10 +0530

    feat: add navigation section with quick links to Footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -50,8 +50,75 @@ export default function Footer() {
             </p>
           </div>
 
-          {/* Column 2: Placeholder for Phase 3 */}
-          <div>{/* Navigation section will be added in Phase 3 */}</div>
+          {/* Column 2: Navigation */}
+          <div>
+            <h3
+              style={{
+                fontSize: 18,
+                fontWeight: 600,
+                color: "var(--color-text-primary)",
+                margin: "0 0 12px 0",
+              }}
+            >
+              Quick Links
+            </h3>
+            <ul
+              style={{
+                listStyle: "none",
+                padding: 0,
+                margin: 0,
+              }}
+            >
+              <li style={{ marginBottom: 8 }}>
+                <Link
+                  href="/browse"
+                  style={{
+                    fontSize: 14,
+                    color: "var(--color-text-secondary)",
+                    textDecoration: "none",
+                  }}
+                >
+                  Browse Produce
+                </Link>
+              </li>
+              <li style={{ marginBottom: 8 }}>
+                <Link
+                  href="/auth"
+                  style={{
+                    fontSize: 14,
+                    color: "var(--color-text-secondary)",
+                    textDecoration: "none",
+                  }}
+                >
+                  Farmer Login
+                </Link>
+              </li>
+              <li style={{ marginBottom: 8 }}>
+                <Link
+                  href="/profile"
+                  style={{
+                    fontSize: 14,
+                    color: "var(--color-text-secondary)",
+                    textDecoration: "none",
+                  }}
+                >
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/"
+                  style={{
+                    fontSize: 14,
+                    color: "var(--color-text-secondary)",
+                    textDecoration: "none",
+                  }}
+                >
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
 
           {/* Column 3: Placeholder for Phase 4 */}
           <div>
